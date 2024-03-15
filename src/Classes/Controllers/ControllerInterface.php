dd9d618dcfbda9b6f11d64f30895420b6cd99012
SidAlex <palach.orl@mail.ru>
2024-03-15 15:46:45 +0300

    controllerInterface

---
+++ src/Classes/Controllers/ControllerInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Sidalex\SwooleApp\Classes\Controllers;
+
+use Sidalex\SwooleApp\Application;
+use Swoole\Http\Server;
+
+interface ControllerInterface
+{
+    /**
+     * @param \Swoole\Http\Request $request
+     * @param \Swoole\Http\Response $response
+     * @param array<string> $uri_params /v1/{module}/{entity} /v1/Accounts/123 ['module' => 'Accounts', 'entity' => '123']
+     */
+    public function __construct(\Swoole\Http\Request $request, \Swoole\Http\Response $response, array $uri_params=[]);
+
+    public function execute(): \Swoole\Http\Response;
+
+    public function setApplication(Application $application, Server $server);
+}
