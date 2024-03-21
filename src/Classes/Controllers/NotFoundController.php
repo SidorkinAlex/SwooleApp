bda857d02a3642b63234b9c88a982b3caf7e7669
SidAlex <palach.orl@mail.ru>
2024-03-21 23:50:23 +0300

    not found controller

--- src/Classes/Controllers/NotFoundController.php
+++ src/Classes/Controllers/NotFoundController.php
@@ -9,34 +9,26 @@ class NotFoundController implements ControllerInterface
 {
 
     private \Swoole\Http\Request $request;
-    private \Swoole\Http\Response $response;
+    private \Swoole\Http\Response $responce;
     /**
      * @var array|string[]
      */
     private array $uri_params;
-    private Application $application;
-    private Server $server;
-    /**
-     * @var array|string[]
-     */
-    private array $errors_message;
 
     public function __construct(\Swoole\Http\Request $request, \Swoole\Http\Response $response, array $uri_params=[])
     {
         $this->request = $request;
-        $this->response = $response;
-        $this->errors_message =$uri_params;
+        $this->responce = $response;
+        $this->uri_params = $uri_params;
     }
 
     public function execute(): \Swoole\Http\Response
     {
-        $errorBuilder = new ErrorResponseBuilder($this->response);
-        return $errorBuilder->errorResponse($this->errors_message['message']);
+        return $this->responce;
     }
 
     public function setApplication(Application $application, Server $server)
     {
-        $this->application = $application;
-        $this->server = $server;
+
     }
 }
