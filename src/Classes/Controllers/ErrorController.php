1d0396474aa6ca0008299e741db5a3bc3d5fad30
SidAlex <palach.orl@mail.ru>
2024-03-21 23:51:43 +0300

    add Error Controller

---
+++ src/Classes/Controllers/ErrorController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sidalex\SwooleApp\Classes\Controllers;
+
+class ErrorController implements ControllerInterface
+{
+
+    protected array $errors_message;
+    protected \Swoole\Http\Response $response;
+    protected \Swoole\Http\Request $request;
+    protected \Sidalex\SwooleApp\Application $application;
+
+    public function __construct(\Swoole\Http\Request $request, \Swoole\Http\Response $response, array $uri_params=[])
+    {
+        $this->request = $request;
+        $this->response = $response;
+        $this->errors_message =$uri_params;
+    }
+
+    public function execute(): \Swoole\Http\Response
+    {
+        //todo ErrorResponseBuilder
+        $errorBuilder = new ErrorResponseBuilder($this->response);
+        return $errorBuilder->errorResponse($this->errors_message['message']);
+    }
+
+    public function setApplication(\Sidalex\SwooleApp\Application $application, \Swoole\Http\Server $server)
+    {
+        $this->application = $application;
+    }
+}
