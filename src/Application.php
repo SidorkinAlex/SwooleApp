aae9b45ef0772dccaf5f696ec82b38983e037218
SidAlex <palach.orl@mail.ru>
2024-04-07 22:31:56 +0300

    add Application taskExecution method

--- src/Application.php
+++ src/Application.php
@@ -6,6 +6,9 @@ use Sidalex\CandidateVacancyEstimationGpt\Classes\Validators\ConfigValidatorInte
 
 use Sidalex\SwooleApp\Classes\Builder\NotFoundControllerBuilder;
 use Sidalex\SwooleApp\Classes\Builder\RoutesCollectionBuilder;
+use Sidalex\SwooleApp\Classes\Tasks\Data\TaskDataInterface;
+use Sidalex\SwooleApp\Classes\Tasks\TaskResulted;
+use Sidalex\SwooleApp\Classes\Utils\Utilities;
 use Sidalex\SwooleApp\Classes\Wrapper\ConfigWrapper;
 use Swoole\Server;
 
@@ -15,12 +18,12 @@ class Application
     private ConfigWrapper $config;
     private array $routesCollection;
 
-    public function __construct(\stdClass $configPath, array $ConfigValidationList=[])
+    public function __construct(\stdClass $configPath, array $ConfigValidationList = [])
     {
         try {
-            foreach ($ConfigValidationList as $configValidationClassName){
+            foreach ($ConfigValidationList as $configValidationClassName) {
                 $validationClass = new $configValidationClassName;
-                if ($validationClass instanceof ConfigValidatorInterface){
+                if ($validationClass instanceof ConfigValidatorInterface) {
                     $validationClass->validate($configPath);
                 }
             }
@@ -39,7 +42,7 @@ class Application
         $Route_builder = new RoutesCollectionBuilder();
         $itemRouteCollection = $Route_builder->searchInRoute($request, $this->routesCollection);
         if (empty($itemRouteCollection)) {
-            $controller = (new NotFoundControllerBuilder($request, $response,$this->config))->build();
+            $controller = (new NotFoundControllerBuilder($request, $response, $this->config))->build();
         } else {
             $controller = $Route_builder->getController($itemRouteCollection, $request, $response);
         }
@@ -48,15 +51,28 @@ class Application
         unset($controller);
     }
 
-    public function getConfig(): \stdClass
+    public function getConfig(): ConfigWrapper
     {
         return $this->config;
     }
 
-    public function taskExecute($taskId, $reactorId, $data)
+    public function taskExecute(\Swoole\Http\Server $server, $taskId, $reactorId, $data): TaskResulted
     {
-        //todo: create typical logic from task
+        if (!($data instanceof TaskDataInterface)) {
+            return new TaskResulted('error data is not a TaskDataInterface', false);
+        }
+        $TaskExecutorClassName = $data->getTaskClassName();
+        if(Utilities::classImplementInterface($TaskExecutorClassName,'Sidalex\SwooleApp\Classes\Tasks\Executors\TaskExecutorInterface'))
+        {
+            $TaskExecutorClass = new $TaskExecutorClassName($server, $taskId, $reactorId, $data);
+            $result = $TaskExecutorClass->execute();
+        } else {
+            return new TaskResulted('error task Executor not implemented TaskExecutorInterface', false);
+        }
+        if (!($result instanceof TaskResulted)) {
+            return new TaskResulted('error result is not a TaskResulted', false);
+        }
+        return $result;
     }
-
 
 }
