6e852fa9bae378f3e9b5cbd4928a7684500ae78c
SidAlex <palach.orl@mail.ru>
2024-04-10 20:40:49 +0300

    add TaskExecutorInterface

---
+++ src/Classes/Tasks/Executors/TaskExecutorInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sidalex\SwooleApp\Classes\Tasks\Executors;
+
+use Sidalex\SwooleApp\Classes\Tasks\Data\TaskDataInterface;
+use Sidalex\SwooleApp\Classes\Tasks\TaskResulted;
+
+interface TaskExecutorInterface
+{
+    /**
+     * @param \Swoole\Http\Server $server
+     * @param $taskId
+     * @param $reactorId
+     * @param TaskDataInterface $data
+     */
+    public function __construct(\Swoole\Http\Server $server, $taskId, $reactorId, TaskDataInterface $data);
+
+    /**
+     * @return TaskResulted
+     */
+    public function execute(): TaskResulted;
+
+}
