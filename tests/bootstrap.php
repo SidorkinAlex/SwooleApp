2ecfbed6a1108adc0722380f269e85e9a3cfbf19
SidorkinAlex <palach.orl@mail.ru>
2024-04-23 12:28:32 +0300

    add butstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+require_once './vendor/autoload.php';
+require_once './tests/TestData/TestClassFromApplicationTest/TestController.php';
+require_once './tests/TestData/TestClassFromApplicationTest/TestController2.php';
+//require_once './tests/TestData/Swoole/Server.php';
