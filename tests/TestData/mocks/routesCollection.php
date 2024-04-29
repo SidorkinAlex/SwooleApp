65ae6872f9ecb43408ef393f2d8e4518ee44bb8d
SidorkinAlex <palach.orl@mail.ru>
2024-04-29 22:36:59 +0300

    chenge list iten to Desc

--- tests/TestData/mocks/routesCollection.php
+++ tests/TestData/mocks/routesCollection.php
@@ -18,4 +18,23 @@ return [
         'method' => 'POST',
         'ControllerClass' => 'TestController3',
     ],
+    [
+        'route_pattern_list' => ['','api', 'v3', 'collectionsList','Accounts','statuses'],
+        'parameters_fromURI' => [],
+        'method' => 'GET',
+        'ControllerClass' => 'TestController6',
+    ],
+    [
+        'route_pattern_list' => ['','api', 'v3', 'collectionsList','Accounts'],
+        'parameters_fromURI' => [],
+        'method' => 'GET',
+        'ControllerClass' => 'TestController5',
+    ],
+    [
+        'route_pattern_list' => ['','api', 'v3', 'collectionsList',],
+        'parameters_fromURI' => [],
+        'method' => 'GET',
+        'ControllerClass' => 'TestController4',
+    ],
+
 ];
