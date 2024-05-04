c5beef6f1f4a74983ea5fdbf2d0f0a7bc75ad922
SidorkinAlex <palach.orl@mail.ru>
2024-05-04 21:33:58 +0300

    includding class Mok Swoole if testing running in an environment without an installed package

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,7 @@ require_once './vendor/autoload.php';
 require_once './tests/TestData/TestClassFromApplicationTest/TestController.php';
 require_once './tests/TestData/TestClassFromApplicationTest/TestController2.php';
 require_once './tests/TestData/TestClassFromApplicationTest/TestNotValidRoutController.php';
+if(!class_exists('\Swoole\Http\Request'))
+{
+    require_once './tests/TestData/Swoole/Request.php';
+}
