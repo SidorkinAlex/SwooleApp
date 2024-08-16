2f139194e06c141636b7d35e8d6d3dc0a2e0dd93
SidorkinAlex <palach.orl@mail.ru>
2024-08-16 18:12:38 +0300

    fixed utilites

--- src/Classes/Utils/Utilities.php
+++ src/Classes/Utils/Utilities.php
@@ -4,9 +4,10 @@ namespace Sidalex\SwooleApp\Classes\Utils;
 
 class Utilities
 {
-    public static function classImplementInterface($className,$interfaceName):bool {
+    public static function classImplementInterface(string $className, string $interfaceName): bool
+    {
         $interfaceCollection = class_implements($className);
-        if (in_array($interfaceName,$interfaceCollection)) {
+        if (is_array($interfaceCollection) && in_array($interfaceName, $interfaceCollection)) {
             return true;
         } else {
             return false;
