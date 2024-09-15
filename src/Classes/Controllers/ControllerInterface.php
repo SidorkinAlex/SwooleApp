7f0f5c67f1e29b3ff2a9b8f19018b36a08678ca5
SidAlex <palach.orl@mail.ru>
2024-09-15 12:39:24 +0300

    add return type from setApplication method to Controller Interrface

--- src/Classes/Controllers/ControllerInterface.php
+++ src/Classes/Controllers/ControllerInterface.php
@@ -12,9 +12,9 @@ interface ControllerInterface
      * @param \Swoole\Http\Response $response
      * @param array<string> $uri_params /v1/{module}/{entity} /v1/Accounts/123 ['module' => 'Accounts', 'entity' => '123']
      */
-    public function __construct(\Swoole\Http\Request $request, \Swoole\Http\Response $response, array $uri_params=[]);
+    public function __construct(\Swoole\Http\Request $request, \Swoole\Http\Response $response, array $uri_params = []);
 
     public function execute(): \Swoole\Http\Response;
 
-    public function setApplication(Application $application, Server $server);
+    public function setApplication(Application $application, Server $server): void;
 }
